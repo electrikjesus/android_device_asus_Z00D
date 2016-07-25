c800268acf06558ec85b581e2d6125653f2024e3
electrikjesus <electrikjesus@gmail.com>
2016-07-25 05:53:46 -0700

    Blissified cm.mk

--- bliss.mk
+++ bliss.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_WIDTH := 720
 REF_PRODUCT_NAME := redhookbay
 TARGET_BOARD_PLATFORM := clovertrail
 
-# Inherit some common CM stuff.
+# Inherit some common Bliss stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00D/device.mk)
@@ -18,7 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z00D/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z00D
+PRODUCT_NAME := bliss_Z00D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z00D
 PRODUCT_MANUFACTURER := asus
